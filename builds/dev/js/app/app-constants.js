d7fbd7f0e3af8b1d1d06fa1818c42ceec4311e28
Carl Hunt <cwhunt@calpoly.edu>
2016-01-15 10:18:24 -0800

    Added site_url parameter to AUTHORIZE_URL.

--- builds/dev/js/app/app-constants.js
+++ builds/dev/js/app/app-constants.js
@@ -3,5 +3,5 @@
 
   angular
     .module('app')
-    .constant('AUTHORIZE_URL', 'https://sandbox.orcid.org/oauth/authorize?client_id=APP-UI9MH0Q8TO2Q3IE5&response_type=code&scope=/authenticate&redirect_uri=https://example-orcid-api.herokuapp.com/auth')
+    .constant('AUTHORIZE_URL', 'https://sandbox.orcid.org/oauth/authorize?client_id=APP-UI9MH0Q8TO2Q3IE5&response_type=code&scope=/authenticate&redirect_uri=https://example-orcid-api.herokuapp.com/auth&site_url=https://example-orcid.firebaseapp.com/')
 }());
